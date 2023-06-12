e1129c29582a4590df55bb24296a9fef3ab58c32
rlguswn <0318joo@naver.com>
2023-06-12 10:42:18 +0900

    텍스트를 입력하지 않고 버튼 클릭시 경고 출력

--- assets/js/question.js
+++ assets/js/question.js
@@ -9,8 +9,16 @@ const $button = document.querySelector('.makePlan')
 // 사용자 입력을 토대로 질문 텍스트 작성
 $button.addEventListener('click', e =>{
     e.preventDefault()
-
-    const userInputData = (`
+    
+    if ($dateDepart.value.trim() === "") {
+        alert("여행을 출발하는 날짜를 입력해주세요.")
+    } else if ($dateArrive.value.trim() === "") {
+        alert("여행에서 돌아오는 날짜를 입력해주세요.")
+    } else if ($destination.value.trim() === "") {
+        alert("원하는 여행지를 입력해주세요.")
+    }
+    else {
+        const userInputData = (`
         ${$dateDepart.value}, 
         ${$dateArrive.value}, 
         ${$attraction.value}, 
@@ -20,4 +28,5 @@ $button.addEventListener('click', e =>{
     console.log(userInputData)
 
     chatGptAPI(userInputData)
+    }
 })
