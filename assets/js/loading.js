e5d2cf320937082fa6f81a749430b201ad9cc4c7
rlguswn <0318joo@naver.com>
2023-06-13 11:27:16 +0900

    로딩스피너에 대한 주석 작성

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,8 +1,10 @@
+// 로딩스피너 가시화
 export function showLoadingSpinner() {
     const spinner = document.getElementById("loading-spinner")
     spinner.style.display = "block"
 }
 
+// 로딩스피너 비가시화
 export function hideLoadingSpinner() {
     const spinner = document.getElementById("loading-spinner")
     spinner.style.display = "none"
